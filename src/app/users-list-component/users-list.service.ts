1861c9d3c2141fcf1a2e703ce6f82fac46d552bb
WolfgangTheDrug <bardak_widz@wp.pl>
2024-10-27 02:55:09 +0200

    Experimental commit

--- src/app/users-list-component/users-list.service.ts
+++ src/app/users-list-component/users-list.service.ts
@@ -2,11 +2,13 @@ import {inject, Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {UserModel} from "./user.model";
+import {UserDTOModel} from "./user-DTO";
 
 @Injectable({providedIn: 'root'})
 export class UsersListService {
   private readonly API_URL: string = 'https://fakestoreapi.com/users';
   private readonly HTTP_CLIENT: HttpClient = inject(HttpClient);
+
 
   getAll(): Observable<UserModel[]> {
     return this.HTTP_CLIENT.get<UserModel[]>(this.API_URL)
